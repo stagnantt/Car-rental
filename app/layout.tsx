b6faf9d090f5c1cff8317a080dfcc7b043b7b968
z1xc1 <125942659+z1xc1@users.noreply.github.com>
2024-05-15 01:04:45 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <SignedIn>
+      {children}
+      </SignedIn>
+      <SignedOut>
+        <SignIn  routing="hash" />
+      </SignedOut>
+        
+        
+        </body>
+      
     </html>
+    </ClerkProvider>
   );
 }
